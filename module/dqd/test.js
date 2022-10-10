67f20ad803907d7b1c3bbed1eb855c468ff6406d
Ryan <wtmanutd@163.com>
2022-10-10 21:05:15 +0800

    dqd

--- module/dqd/test.js
+++ module/dqd/test.js
@@ -1,13 +1,4 @@
 const res = $response;
-res.body = JSON.stringify({
-  "code": 10001,
-  "message": "success",
-  "data": { "limit": false },
-  "success": true
-})
-$done(res)
-
-const res = $response;
 const body = JSON.parse($response.body)
 body.data = { "limit": false }
 
