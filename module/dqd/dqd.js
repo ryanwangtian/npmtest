c3e114f848f54a8494389f2de2795425fcf9f660
Ryan <wtmanutd@163.com>
2022-10-10 20:37:13 +0800

    dqd

---
+++ module/dqd/dqd.js
@@ -0,0 +1,7 @@
+const BLACK_LIST = ['乒', '篮', 'NBA', 'CBA', '排', 'NBL', '女', '英雄联盟']
+const res = $response;
+const body = JSON.parse($response.body)
+body.list = body.list.filter(item => !BLACK_LIST.find(text => item.match_title && item.match_title.includes(text)))
+
+res.body = JSON.stringify(body);
+$done(res);
