466890bf84aab3af291e65e6937fe585f691475c
Ryan <wtmanutd@163.com>
2022-10-27 14:16:21 +0800

    dqd

--- module/dqd/dqd.js
+++ module/dqd/dqd.js
@@ -1,4 +1,4 @@
-const BLACK_LIST = ['乒', '篮', 'NBA', 'CBA', '排', 'NBL', '女', '英雄联盟']
+const BLACK_LIST = ['乒', '篮', 'NBA', 'CBA', '排', 'NBL', '女', '英雄联盟', '中甲', '中乙', 'U21联赛']
 const res = $response;
 const body = JSON.parse($response.body)
 body.list = body.list.filter(item => !BLACK_LIST.find(text => item.match_title && item.match_title.includes(text)))
