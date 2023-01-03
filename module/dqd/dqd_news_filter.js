4d9afb39aa86eb643f1e1def030ee9a644d148d6
Ryan <wtmanutd@163.com>
2023-01-03 16:35:16 +0800

    update

--- module/dqd/dqd_news_filter.js
+++ module/dqd/dqd_news_filter.js
@@ -1,6 +1,8 @@
 const res = $response;
 const body = JSON.parse($response.body);
-// body.articles = body.articles.filter((item) => item.category === "足球");
+body.articles = Array.isArray(body.articles)
+  ? body.articles.filter((item) => item.category === "足球")
+  : body.articles;
 
 res.body = JSON.stringify(body);
 $done(res);
